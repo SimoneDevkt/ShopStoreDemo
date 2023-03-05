727aa9105fea1266e6f14c4ff15603f720883b65
Simone <simonep97@gmail.com>
2023-03-05 22:42:12 +0100

    add db

---
+++ database/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE shop;
+
+USE shop;
+
+CREATE TABLE product (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE cart (
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
