2e76c2b181b5bd583f63a54729ef120c49ef4d31
Simone <simonep97@gmail.com>
2023-03-12 17:43:41 +0100

    implement login mock

--- backend/routes/login.ts
+++ backend/routes/login.ts
@@ -1,9 +1,26 @@
 import { Request, Response, Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-router.post('/login', (req: Request, res: Response) => {
-    res.send('Hello login!');
+
+import promisePool from '../plugins/mysql';//for mocking
+
+router.post('/login', async (req: Request, res: Response) => {    
+    //TODO: add login logic. now we use a mock with user 1
+    const id = 1
+    const name = 'Jhon Doe'
+    try {
+        const [rows,fields] = await promisePool.query('SELECT * FROM users WHERE id = ?', id);
+        if(rows.length === 0){
+            await promisePool.query('INSERT INTO users (id, name) VALUES (?, ?);', [id, name]);
+        }
+        const token = jwt.sign({ userId: 1 }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500)
+        res.send({Error: 500})
+    }
 });
 
 export default router;
